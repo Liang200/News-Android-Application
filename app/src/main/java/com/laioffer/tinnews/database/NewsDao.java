c99c5d8175d93aca79cb44fa86842bf03f8a50d0
Tianao Xu <tianaoxu@Tianaos-MBP.fios-router.home>
2019-02-24 01:09:26 -0500

    save page with MVP model

--- app/src/main/java/com/laioffer/tinnews/database/NewsDao.java
+++ app/src/main/java/com/laioffer/tinnews/database/NewsDao.java
@@ -2,13 +2,20 @@ package com.laioffer.tinnews.database;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
 
 import com.laioffer.tinnews.retrofit.response.News;
 
-//7.4
+import java.util.List;
+
+import io.reactivex.Flowable;
+
 @Dao
 public interface NewsDao {
-
     @Insert
     void insertNews(News news);
+
+    //3.1
+    @Query("SELECT * FROM news")
+    Flowable<List<News>> getAll();
 }
